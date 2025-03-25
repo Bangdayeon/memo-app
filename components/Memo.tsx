6f12ef61a295e60f88961cb94d9b5664634ca999
Bangdayeon <dybang00@gmail.com>
2025-03-26 01:37:45 +0900

    스타일 추가

--- components/Memo.tsx
+++ components/Memo.tsx
@@ -1,15 +1,16 @@
 import {useState} from "react";
+import styles from "../styles/Memo.module.css";
 
 const Memo = () => {
     const [text, setText] = useState("");
 
     return (
-        <div className="memo-container">
+        <div className={styles.memoContainer}>
             <textarea
             value={text}
             onChange={(e)=> setText(e.target.value)}
             placeholder="메모를 입력하세요..."
-            className="memo-textarea"
+            className={styles.memoTextarea}
             />
         </div>
     );
